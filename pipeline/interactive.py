3f1e38879bcb807c0eeb924072b10b51155b0522
Kohaku Namori <2633047525@qq.com>
2026-08-27 03:44:27 -0400

    Use headless video-auth interactive app

--- pipeline/interactive.py
+++ pipeline/interactive.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 
 import sys
 
-from .interactive_app import InteractiveWizard
 from .i18n import initialize_interactive
-from .i18n_menu import install_language_menu
+from .interactive_video_auth import InteractiveWizard
 
 
 def main() -> None:
@@ -14,7 +13,6 @@ def main() -> None:
         cli_main()
         return
     initialize_interactive()
-    install_language_menu()
     InteractiveWizard().home()
 
 
