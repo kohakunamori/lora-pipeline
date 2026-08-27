bbe9754e1b2de109be6a4dca37f5e10738f28054
Kohaku Namori <2633047525@qq.com>
2026-08-27 10:10:20 -0400

    Use metadata-aware web workspace

--- pipeline/web_full.py
+++ pipeline/web_full.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from .web_entry import FinalHandler as FullHandler
-from .web_entry import main, make_server, serve
+from .web_metadata import MetadataHandler as FullHandler
+from .web_metadata import main, make_server, serve
 
 __all__ = ["FullHandler", "make_server", "serve", "main"]
 
