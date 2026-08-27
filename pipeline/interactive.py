ed18fec204c338ed726faa8caa626afaf5a4d478
Kohaku Namori <2633047525@qq.com>
2026-08-27 02:53:26 -0400

    Expose language switching from interactive home

--- pipeline/interactive.py
+++ pipeline/interactive.py
@@ -4,6 +4,7 @@ import sys
 
 from .interactive_app import InteractiveWizard
 from .i18n import initialize_interactive
+from .i18n_menu import install_language_menu
 
 
 def main() -> None:
@@ -13,6 +14,7 @@ def main() -> None:
         cli_main()
         return
     initialize_interactive()
+    install_language_menu()
     InteractiveWizard().home()
 
 
