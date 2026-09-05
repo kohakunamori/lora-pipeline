8525546fa0a125be943fa4790227e73414fe6cd4
Kohaku Namori <2633047525@qq.com>
2026-09-05 18:56:14 +0900

    Delete obsolete semantic project hook shell

--- pipeline/semantic_project_hooks.py
+++
@@ -1,5 +0,0 @@
-"""Deprecated compatibility module.
-
-Project snapshot composition is explicit in ``training_config.create_project_from_training_config``.
-Importing this module intentionally has no side effects.
-"""
